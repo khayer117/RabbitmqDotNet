ca0e9460ead4e033c1c2e38f91eda16b5281d2e5
khayer <khayer@adfenix.com>
2017-09-25 01:19:49 +0600

    created projects

---
+++ RabbitmqConsumer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitmqDotNetCore;
+
+namespace RabbitmqConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+
+                    var commandTypeBytes = ea.BasicProperties.Headers["commandType"] as byte[];
+                    var commandType = Encoding.UTF8.GetString(commandTypeBytes);
+
+                    //var customerCommand = JsonConvert.DeserializeObject<UpdateCustomerCommand>(message);
+                    var cType = Type.GetType(commandType);
+                    var customerCommand = JsonConvert.DeserializeObject(message, cType);
+                    
+                    Console.WriteLine(" [x] Received {0}", "Tested");
+                };
+                channel.BasicConsume(queue: "hello",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ RabbitmqProducer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitmqDotNetCore;
+
+namespace RabbitmqProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var customerUpdate = new UpdateCustomerCommand()
+                {
+                    CustomerId = 101,
+                    CustomerName = "Test Name"
+                };
+
+                var basicProperties = channel.CreateBasicProperties();
+                basicProperties.Headers = new Dictionary<string, object>
+                {
+                    {"commandType", customerUpdate.GetType().AssemblyQualifiedName}
+                };
+
+
+                string message = JsonConvert.SerializeObject(customerUpdate);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: basicProperties,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
