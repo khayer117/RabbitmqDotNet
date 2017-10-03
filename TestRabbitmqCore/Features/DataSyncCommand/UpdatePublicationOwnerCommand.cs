5900c788434ff71ba8b17c89bf2a00f12e75079c
Abul Khayer <khayer@adfenix.com>
2017-10-03 12:07:39 +0600

    created feature folder

--- TestRabbitmqCore/Features/DataSyncCommand/UpdatePublicationOwnerCommand.cs
+++ TestRabbitmqCore/Features/DataSyncCommand/UpdatePublicationOwnerCommand.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RabbitmqDotNetCore.Infrastructure;
 
-namespace RabbitmqDotNetCore.Infrastructure
+namespace RabbitmqDotNetCore.Features
 {
     public class UpdatePublicationOwnerCommand:IQueueCommand
     {
